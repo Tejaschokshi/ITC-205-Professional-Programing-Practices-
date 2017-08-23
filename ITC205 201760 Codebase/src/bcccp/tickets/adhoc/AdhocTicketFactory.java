fc5332c8c72e988a03857057f9a325f823ee596d
zainubawan <30613828+zainubawan@users.noreply.github.com>
2017-08-23 23:16:18 +1000

    Update AdhocTicketFactory.java

--- ITC205 201760 Codebase/src/bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ ITC205 201760 Codebase/src/bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -4,7 +4,7 @@ public class AdhocTicketFactory implements IAdhocTicketFactory {
 
 	@Override
 	public IAdhocTicket make(String carparkId, int ticketNo) {
-		// TODO Auto-generated method stub
+		//returning the value for AdhocTickets method
 		return AdhocTicket;
 	}
 
