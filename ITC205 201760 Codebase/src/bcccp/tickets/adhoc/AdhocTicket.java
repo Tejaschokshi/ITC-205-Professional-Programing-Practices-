6eac5d2a3d4d60ba684c4887af4efd5b4eee97e2
kotiansmeet <31027546+kotiansmeet@users.noreply.github.com>
2017-08-24 20:08:32 +1000

    Comment*
    using a method to return the value describing the date and time the charges were paid.

--- ITC205 201760 Codebase/src/bcccp/tickets/adhoc/AdhocTicket.java
+++ ITC205 201760 Codebase/src/bcccp/tickets/adhoc/AdhocTicket.java
@@ -98,6 +98,7 @@ public class AdhocTicket implements IAdhocTicket {
 	@Override
 	public long getPaidDateTime() {
 		// TODO Auto-generated method stub
+		// this set of code uses a method to return the value 
 		return paidDateTime;;
 	}
 
