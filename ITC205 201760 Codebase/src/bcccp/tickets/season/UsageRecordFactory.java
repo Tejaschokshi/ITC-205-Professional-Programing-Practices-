9ecfbd789c66ae4a5c8d78acb040f51963ebb2d3
zainubawan <30613828+zainubawan@users.noreply.github.com>
2017-08-25 02:01:51 +1000

    zainab - calculating IUsageRecord result

--- ITC205 201760 Codebase/src/bcccp/tickets/season/UsageRecordFactory.java
+++ ITC205 201760 Codebase/src/bcccp/tickets/season/UsageRecordFactory.java
@@ -4,6 +4,7 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
+		IUsageRecord record = ticketId + startDateTime
 		// obtaining value from method after method is called
 		return UsageRecord;
 	}
