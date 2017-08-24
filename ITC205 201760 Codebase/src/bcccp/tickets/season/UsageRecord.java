17b7db9b58feba435a9f309e92f604a64c8b9a06
zainubawan <30613828+zainubawan@users.noreply.github.com>
2017-08-25 00:38:46 +1000

    Denoting instance variable

--- ITC205 201760 Codebase/src/bcccp/tickets/season/UsageRecord.java
+++ ITC205 201760 Codebase/src/bcccp/tickets/season/UsageRecord.java
@@ -9,6 +9,9 @@ public class UsageRecord implements IUsageRecord {
 	
 	
 	public UsageRecord(String ticketId, long startDateTime) {
+		this.ticketId = ticketId;
+		this.startDateTime = startDateTime;
+	}
 
 
 
