2b53c19da03576fe1c81b58b1f0600b64939fc1d
susan2392 <30614035+susan2392@users.noreply.github.com>
2017-08-25 14:32:59 +1000

    implement constructor

--- ITC205 201760 Codebase/src/bcccp/tickets/adhoc/AdhocTicketDAO.java
+++ ITC205 201760 Codebase/src/bcccp/tickets/adhoc/AdhocTicketDAO.java
@@ -8,7 +8,7 @@ public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 	private int currentTicketNo;
 
 	
-	//This set of code implement methodsi.e. get and set method to retrieve and get value for the ticket,barcode.
+	
 	public AdhocTicketDAO(IAdhocTicketFactory factory) {
 		IAdhocTicketDAO.factory= factory;
 		this.setIAdhocTicketDAO(factory); //addition of this  results in invoking of another method
@@ -16,35 +16,30 @@ public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 	
 		
 		public String getAdhocTicketDAO(){   //get methods
-			return AdhocTicketDAO;          //returns the value
-			
-		}
+			return AdhocTicketDAO; 
 		//TODO Implement constructor
 	}
 
 
-          //this set of code get the ticket through get method and returns the value
+
 	@Override
 	public IAdhocTicket createTicket(String carparkId) {
-		public void setIAdhocTicketDAO(carparkId);   //set method
-		 get.AdhocTicketDAO(carparkId); // TODO Auto-generated method stub
-		return null;  //returns null
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 
-        // this set of code helps to find out the ticket by barcode and return the value
+
 	@Override
 	public IAdhocTicket findTicketByBarcode(String barcode) {
-		get.AdhocTicketDAO(barcode); //get method which gets the barcode
 		// TODO Auto-generated method stub
-		return null;         //returns value 
+		return null;
 	}
 
 
-       //this set of code helps to list all of the tickets and return value
+
 	@Override
 	public List<IAdhocTicket> getCurrentTickets() {
-		get.AdhocticketDAO(tickets);//get method that returns the all the tickets value 
 		// TODO Auto-generated method stub
 		return null;
 	}
